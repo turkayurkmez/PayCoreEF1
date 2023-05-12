16ceb1e1f5fdd7d225b34e8f3332928aae44cbca
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-05-12 20:04:00 +0300

    first EF Core App initialized

---
+++ firstApp/firstApp/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace firstApp.Models
+{
+    //Convention Over Configuration
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? PublishedOn { get; set; }
+        public int AuthorId { get; set; }
+        //Navigation Property:
+        public Author Author { get; set; }
+
+
+
+
+    }
+}
